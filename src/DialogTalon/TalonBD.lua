858957cd0d6a0edb4ba8f75ab31cbdcd0d120139
jsore <m79054050200@gmail.com>
2021-02-20 16:59:43 +0300

    Добавил базу данных талантов только с двумя талантами Тралла

---
+++ src/DialogTalon/TalonBD.lua
@@ -0,0 +1,15 @@
+TalonBD = {
+    Trall = {--Тралл
+        SpiritWolf = { -- талант1
+            icon = "ReplaceableTextures\\CommandButtons\\BTNSpiritWolf.blp",
+            description = "Призывает волка",
+            level = 0
+        },
+        ThunderStrike = { -- талант2
+            icon = "ReplaceableTextures\\CommandButtons\\BTNChainLightning.blp",
+            description = "Молот наносит урон молнией",
+            level = 0
+        },
+    },
+    BLADEMASTER={} -- повторяем
+}
