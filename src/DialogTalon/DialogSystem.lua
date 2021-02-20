280059d794c259bdc83a8a4a12d6aecbff69897d
jsore <m79054050200@gmail.com>
2021-02-20 17:05:07 +0300

    Добавил код для распознования таланта Тралла

--- src/DialogTalon/DialogSystem.lua
+++ src/DialogTalon/DialogSystem.lua
@@ -1,3 +1,10 @@
 function CreateDialogTalon(godName)
     BlzLoadTOCFile("SystemGeneric\\Main.toc")
+
+    local title = ""
+    local talons = {}
+    if godName == "Trall" then
+        title = "Дары Тралла"
+        talons = TalonBD.Trall
+    end
 end
