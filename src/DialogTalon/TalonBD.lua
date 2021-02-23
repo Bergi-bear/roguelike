8c8c37d894db116fc50b2eb77b799b06a0c7164c
jsore <m79054050200@gmail.com>
2021-02-23 16:57:19 +0300

    Добавил 10 тестовых талантов для Даров Тралла

--- src/DialogTalon/TalonBD.lua
+++ src/DialogTalon/TalonBD.lua
@@ -1,14 +1,74 @@
 TalonBD = {
     Trall = {--Тралл
-        SpiritWolf = { -- талант1
+        [1] = { -- талант1
             icon = "ReplaceableTextures\\CommandButtons\\BTNSpiritWolf.blp",
+            name = "Призыв волка",
             description = "Призывает волка",
-            level = 0
+            level = 0,
+            rarity = "normal"
         },
-        ThunderStrike = { -- талант2
+        [2] = { -- талант2
             icon = "ReplaceableTextures\\CommandButtons\\BTNChainLightning.blp",
+            name = "Удар молнией",
             description = "Молот наносит урон молнией",
-            level = 0
+            level = 1,
+            rarity = "normal"
+        },
+        [3] = { -- талант2
+            icon = "ReplaceableTextures\\CommandButtons\\BTNChainLightning.blp",
+            name = "Талант 3",
+            description = "Молот наносит урон молнией",
+            level = 2,
+            rarity = "normal"
+        },
+        [4] = { -- талант2
+            icon = "ReplaceableTextures\\CommandButtons\\BTNChainLightning.blp",
+            name = "Талант 4",
+            description = "Молот наносит урон молнией",
+            level = 0,
+            rarity = "normal"
+        },
+        [5] = { -- талант2
+            icon = "ReplaceableTextures\\CommandButtons\\BTNChainLightning.blp",
+            name = "Талант 5",
+            description = "Молот наносит урон молнией",
+            level = 1,
+            rarity = "normal"
+        },
+        [6] = { -- талант2
+            icon = "ReplaceableTextures\\CommandButtons\\BTNChainLightning.blp",
+            name = "Талант 6",
+            description = "Молот наносит урон молнией",
+            level = 2,
+            rarity = "epic"
+        },
+        [7] = { -- талант2
+            icon = "ReplaceableTextures\\CommandButtons\\BTNChainLightning.blp",
+            name = "Талант 7",
+            description = "Молот наносит урон молнией",
+            level = 1,
+            rarity = "rare"
+        },
+        [8] = { -- талант2
+            icon = "ReplaceableTextures\\CommandButtons\\BTNChainLightning.blp",
+            name = "Талант 8",
+            description = "Молот наносит урон молнией",
+            level = 2,
+            rarity = "normal"
+        },
+        [9] = { -- талант2
+            icon = "ReplaceableTextures\\CommandButtons\\BTNChainLightning.blp",
+            name = "Талант 9",
+            description = "Молот наносит урон молнией",
+            level = 0,
+            rarity = "normal"
+        },
+        [10] = { -- талант2
+            icon = "ReplaceableTextures\\CommandButtons\\BTNChainLightning.blp",
+            name = "Талант 10",
+            description = "Молот наносит урон молнией",
+            level = 2,
+            rarity = "normal"
         },
     },
     BLADEMASTER={} -- повторяем
