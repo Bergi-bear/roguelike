d088a3a0fe3494fd55fe976d78cf8e2036e22087
jsore <m79054050200@gmail.com>
2021-02-25 09:39:12 +0300

    Добавил функцию перемешивания

--- src/DialogTalon/AdditionalFunctions.lua
+++ src/DialogTalon/AdditionalFunctions.lua
@@ -22,3 +22,20 @@ function SmoothWindowAppearance(frame, state)
         end
     end)
 end
+
+-- Перемешивание списка
+function swap(array, index1, index2)
+    array[index1], array[index2] = array[index2], array[index1]
+end
+
+function shake(array)
+    local counter = #array
+
+    while counter > 1 do
+        local index = math.random(counter)
+
+        swap(array, index, counter)
+        counter = counter - 1
+    end
+end
+
